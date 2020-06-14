cd63a054f39bf66ca70b3edd866381065e318459
wsw-black <2225180543@qq.com>
2020-06-14 13:47:47 +0800

    完成商品模块开发

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/global.css'
-import { Form, FormItem, Input, Button, Message, Container, Aside, Main, Header, Submenu, MenuItemGroup, MenuItem, Menu, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane } from 'element-ui'
+import { Form, FormItem, Input, Button, Message, Container, Aside, Main, Header, Submenu, MenuItemGroup, MenuItem, Menu, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, CheckboxGroup, Checkbox, Upload, Step, Steps } from 'element-ui'
 import axios from 'axios'
 import ZKTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+Vue.use(VueQuillEditor /* { default global options } */)
+
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // axios 请求拦截
 axios.interceptors.request.use(config => {
@@ -46,6 +53,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
+Vue.use(Step)
+Vue.use(Steps)
 Vue.prototype.$message = Message
 Vue.config.productionTip = false
 Vue.prototype.$confirm = MessageBox.confirm
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Right from '../components/power/Right.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
+import GoodsList from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -24,7 +26,9 @@ const router = new VueRouter({
         { path: '/rights', component: Right },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+        { path: '/params', component: Params },
+        { path: '/goods', component: GoodsList },
+        { path: '/goods/add', component: Add }
       ]
     }
   ]
